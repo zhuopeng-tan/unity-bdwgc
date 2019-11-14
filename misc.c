061d852b24f0128930fa7c4e24ec9d6529cf0eda
Josh Peterson <joshuap@unity3d.com>
2019-11-14 09:41:54 -0500

    Only call a Windows-specific function on Windows

--- misc.c
+++ misc.c
@@ -1430,11 +1430,14 @@ GC_API void GC_CALL GC_enable_incremental(void)
 #     endif
         GC_clear_exclusion_table();
         memset(&GC_arrays, 0, sizeof(GC_arrays));
+#     if defined(MSWIN32) || defined(MSWINCE)
         GC_win32_free_heap();
+#     endif
         GC_clear_freelist();
         GC_clear_bottom_indices();
         GC_clear_finalizable_object_table();
         GC_reset_mark_statics();
+        GC_reset_default_push_other_roots();
     }
   }
 
